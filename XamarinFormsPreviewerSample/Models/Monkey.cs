6301270f816826181e58384e29690dd5738d0cde
goto63 <tgoto63@gmail.com>
2016-05-18 21:45:49 +0700

    Add MonkeysPage.

---
+++ XamarinFormsPreviewerSample/Models/Monkey.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace XamarinFormsPreviewerSample
+{
+    public class Monkey
+    {
+        public Monkey()
+        {
+        }
+    }
+}
+
---
+++ XamarinFormsPreviewerSample/ViewModels/MonkeysViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace XamarinFormsPreviewerSample
+{
+    public class MonkeysViewModel
+    {
+        public MonkeysViewModel()
+        {
+        }
+    }
+}
+
